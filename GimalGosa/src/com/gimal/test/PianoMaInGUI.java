4b0374c4712a0dfb5ef4cbe4ac9f6abf78d70968
Lee2ee <e990727@gmail.com>
2022-06-07 00:41:33 +0900

    --클래스 정리--

--- GimalGosa/src/com/gimal/test/PianoMaInGUI.java
+++ GimalGosa/src/com/gimal/test/PianoMaInGUI.java
@@ -2,14 +2,22 @@ package com.gimal.test;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -20,27 +28,39 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
+import javax.swing.KeyStroke;
 import javax.swing.border.Border;
 
-class Piano extends JFrame implements ActionListener {
+class Piano extends JFrame {
+
+	JCheckBox note_check[][] = new JCheckBox[7][20];
+	JMenuItem mi[] = new JMenuItem[4];
+	JButton note_button[] = new JButton[7];
+	JButton control_button[] = new JButton[3];
+
+	File sound_do = new File("do.wav");
+	File sound_re = new File("re.wav");
+	File sound_mi = new File("mi.wav");
+	File sound_fa = new File("fa.wav");
+	File sound_sol = new File("sol.wav");
+	File sound_la = new File("la.wav");
+	File sound_ti = new File("ti.wav");
+
 	public Piano() {
+
 		Container ct = getContentPane();
 
 		JPanel northP, north_1, north_2, centerP, southP;
 		String note_str[] = { "도", "레", "미", "파", "솔", "라", "시" };
 		String mi_str[] = { "Save", "Open", "Reset", "Logout" };
-		String control_str[] = {"STOP", "PLAY", "PAUSE"};
-		JMenuItem mi[] = new JMenuItem[4];
-		JButton note_button[] = new JButton[7];
-		JCheckBox note_check[] = new JCheckBox[7];
-		JButton control_button[] = new JButton[3];
-		
+		String control_str[] = { "STOP", "PLAY", "PAUSE" };
+
 		// 메뉴바
 		JMenuBar menu = new JMenuBar();
 		JMenu fileMenu = new JMenu("File");
 		for (int i = 0; i < mi.length; i++) {
 			mi[i] = new JMenuItem(mi_str[i]);
-			mi[i].addActionListener(this);
+			mi[i].addActionListener(new Menu_Event());
 			fileMenu.add(mi[i]);
 		}
 		menu.add(fileMenu);
@@ -55,23 +75,26 @@ class Piano extends JFrame implements ActionListener {
 		for (int i = 0; i < note_button.length; i++) {
 			note_button[i] = new JButton(note_str[i]);
 			note_button[i].setPreferredSize(new Dimension(200, 60));
-			note_button[i].addActionListener(this);
+			note_button[i].addActionListener(new Note_ButtonEvent());
 			north_1.add(note_button[i]);
 		}
-		
 
 		// 메인 (체크박스)
 		north_2 = new JPanel();
 		north_2.setLayout(new GridLayout(7, 1, 10, 10));
+
 		for (int i = 0; i < 7; i++) {
 			for (int j = 0; j < 20; j++) {
-				note_check[i] = new JCheckBox(note_str[i]);
-				north_2.add(note_check[i]);
+				note_check[i][j] = new JCheckBox(note_str[i]);
+				note_check[i][j].setBorderPainted(true);
+				note_check[i][j].addItemListener(new Note_CheckEvent());
+				north_2.add(note_check[i][j]);
 			}
 		}
+
 		northP.add(north_1, BorderLayout.WEST);
 		northP.add(north_2, BorderLayout.CENTER);
-		
+
 		// 프로그레스 바
 		centerP = new JPanel();
 		centerP.setLayout(new GridLayout(1, 1, 10, 10));
@@ -81,27 +104,26 @@ class Piano extends JFrame implements ActionListener {
 		Border pbborder = BorderFactory.createTitledBorder("진행률...");
 		pb.setBorder(pbborder);
 		centerP.add(pb);
-		
+
 		// 정지 시작 일시정지 버튼
 		southP = new JPanel();
 		southP.setLayout(new GridLayout(1, 3, 100, 10));
 		for (int i = 0; i < control_button.length; i++) {
 			control_button[i] = new JButton(control_str[i]);
 			control_button[i].setPreferredSize(new Dimension(10, 80));
-			control_button[i].addActionListener(this);
+			control_button[i].addActionListener(new Control_ButtonEvent());
 			southP.add(control_button[i]);
 		}
-		
+
 		// 버튼컬러
-		note_button[0].setBackground(new Color(255, 0, 0));		// 빨
-		note_button[1].setBackground(new Color(255, 100, 0));	// 주
-		note_button[2].setBackground(new Color(255, 255, 0));	// 노
-		note_button[3].setBackground(new Color(0, 255, 0));		// 초
-		note_button[4].setBackground(new Color(0, 100, 255));	// 파
-		note_button[5].setBackground(new Color(0, 0, 255));		// 남
-		note_button[6].setBackground(new Color(100, 0, 255));	// 보
-		
-		
+		note_button[0].setBackground(new Color(255, 0, 0)); // 빨
+		note_button[1].setBackground(new Color(255, 100, 0)); // 주
+		note_button[2].setBackground(new Color(255, 255, 0)); // 노
+		note_button[3].setBackground(new Color(0, 255, 0)); // 초
+		note_button[4].setBackground(new Color(0, 100, 255)); // 파
+		note_button[5].setBackground(new Color(0, 0, 255)); // 남
+		note_button[6].setBackground(new Color(100, 0, 255)); // 보
+
 		// 글꼴컬러
 		note_button[0].setForeground(new Color(0, 255, 0));
 		note_button[1].setForeground(new Color(0, 0, 255));
@@ -110,65 +132,302 @@ class Piano extends JFrame implements ActionListener {
 		note_button[4].setForeground(new Color(255, 100, 0));
 		note_button[5].setForeground(new Color(255, 50, 0));
 		note_button[6].setForeground(new Color(255, 255, 0));
-		
+
 		// 글꼴
 		for (int i = 0; i < note_button.length; i++) {
-		note_button[i].setFont(new Font("MapleStory Bold", Font.BOLD, 40));
+			note_button[i].setFont(new Font("MapleStory Bold", Font.BOLD, 40));
 		}
-	
-		
-		
+
 		// 컨테이너에 붙이기
 		ct.add(northP, BorderLayout.NORTH);
 		ct.add(centerP, BorderLayout.CENTER);
 		ct.add(southP, BorderLayout.SOUTH);
-		
+
 		// 여백
-		northP.setBorder(BorderFactory.createEmptyBorder(10 , 10, 10 , 10));
-		centerP.setBorder(BorderFactory.createEmptyBorder(10 , 10, 10 , 10));
-		southP.setBorder(BorderFactory.createEmptyBorder(10 , 10, 10 , 10));
+		northP.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		centerP.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		southP.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
 		// 기본설정
 		setTitle("Piano");
 		setSize(1400, 750);
+		setLocationRelativeTo(null);
 		setVisible(true);
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		String action = e.getActionCommand();
-		// 메뉴바 이벤트
-		if (action == "Save") {
-			JOptionPane.showMessageDialog(null, "Save Test");
-		} else if (action == "Open") {
-			JOptionPane.showMessageDialog(null, "Open Test");
-		} else if (action == "Reset") {
-			JOptionPane.showMessageDialog(null, "Reset Test");
-		} else if (action == "Logout") {
-			JOptionPane.showMessageDialog(null, "Logout Test");
+	class Sound {
+		// note버튼 이벤트 1옥타브 올리고 싶으면 + 붙이기 ex) sound_do+
+
+		public void stream_do() {
+			try {
+				AudioInputStream stream = AudioSystem.getAudioInputStream(sound_do);
+				Clip clip = AudioSystem.getClip();
+				clip.open(stream);
+				clip.start();
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "(도) 음원이 존재하지 않습니다.");
+			}
 		}
 
-		// note버튼 이벤트
-		if (action == "도") {
-			JOptionPane.showMessageDialog(null, "도 Test");
-		} else if (action == "레") {
-			JOptionPane.showMessageDialog(null, "레 Test");
-		} else if (action == "미") {
-			JOptionPane.showMessageDialog(null, "미 Test");
-		} else if (action == "파") {
-			JOptionPane.showMessageDialog(null, "파 Test");
-		} else if (action == "솔") {
-			JOptionPane.showMessageDialog(null, "솔 Test");
-		} else if (action == "라") {
-			JOptionPane.showMessageDialog(null, "라 Test");
-		} else if (action == "시") {
-			JOptionPane.showMessageDialog(null, "시 Test");
+		public void stream_re() {
+			try {
+				AudioInputStream stream = AudioSystem.getAudioInputStream(sound_re);
+				Clip clip = AudioSystem.getClip();
+				clip.open(stream);
+				clip.start();
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "(레) 음원이 존재하지 않습니다.");
+			}
 		}
-		
-		// checkbox 이벤트
-		
+
+		public void stream_mi() {
+			try {
+				AudioInputStream stream = AudioSystem.getAudioInputStream(sound_mi);
+				Clip clip = AudioSystem.getClip();
+				clip.open(stream);
+				clip.start();
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "(미) 음원이 존재하지 않습니다.");
+			}
+		}
+
+		public void stream_fa() {
+			try {
+				AudioInputStream stream = AudioSystem.getAudioInputStream(sound_fa);
+				Clip clip = AudioSystem.getClip();
+				clip.open(stream);
+				clip.start();
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "(파) 음원이 존재하지 않습니다.");
+			}
+		}
+
+		public void stream_sol() {
+			try {
+				AudioInputStream stream = AudioSystem.getAudioInputStream(sound_sol);
+				Clip clip = AudioSystem.getClip();
+				clip.open(stream);
+				clip.start();
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "(솔) 음원이 존재하지 않습니다.");
+			}
+		}
+
+		public void stream_la() {
+			try {
+				AudioInputStream stream = AudioSystem.getAudioInputStream(sound_la);
+				Clip clip = AudioSystem.getClip();
+				clip.open(stream);
+				clip.start();
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "(라) 음원이 존재하지 않습니다.");
+			}
+		}
+
+		public void stream_ti() {
+			try {
+				AudioInputStream stream = AudioSystem.getAudioInputStream(sound_ti);
+				Clip clip = AudioSystem.getClip();
+				clip.open(stream);
+				clip.start();
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "(시) 음원이 존재하지 않습니다.");
+			}
+		}
+
+	}
+
+	class Menu_Event implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			String action = e.getActionCommand();
+			// 메뉴바 이벤트
+			if (action == "Save") {
+				JOptionPane.showMessageDialog(null, "Save Test");
+			} else if (action == "Open") {
+				JOptionPane.showMessageDialog(null, "Open Test");
+			} else if (action == "Reset") {
+
+				for (int i = 0; i < 7; i++) {
+					for (int j = 0; j < 20; j++) {
+						note_check[i][j].setSelected(false);
+					}
+				}
+
+			} else if (action == "Logout") {
+				JOptionPane.showMessageDialog(null, "Logout Test");
+			}
+		}
+	}
+
+	class Note_ButtonEvent implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			String action = e.getActionCommand();
+			Sound sound = new Sound();
+			if (action == "도") {
+				sound.stream_do();
+			} else if (action == "레") {
+				sound.stream_re();
+			} else if (action == "미") {
+				sound.stream_mi();
+			} else if (action == "파") {
+				sound.stream_fa();
+			} else if (action == "솔") {
+				sound.stream_sol();
+			} else if (action == "라") {
+				sound.stream_la();
+			} else if (action == "시") {
+				sound.stream_ti();
+			}
+		}
+	}
+
+	class Note_CheckEvent implements ItemListener {
+		// checkBox 이벤트
+		@Override
+		public void itemStateChanged(ItemEvent e) {
+			// TODO Auto-generated method stub
+			for (int j = 0; j < 20; j++) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					if (e.getSource() == note_check[0][j]) {
+						note_check[0][j].setBackground(new Color(255, 0, 0));
+						note_check[0][j].setForeground(new Color(0, 0, 255));
+					}
+				} else if (e.getStateChange() == ItemEvent.DESELECTED) {
+					if (e.getSource() == note_check[0][j]) {
+						note_check[0][j].setBackground(new Color(238, 238, 238));
+						note_check[0][j].setForeground(new Color(0, 0, 0));
+					}
+				}
+			}
+
+			for (int j = 0; j < 20; j++) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					if (e.getSource() == note_check[1][j]) {
+						note_check[1][j].setBackground(new Color(255, 100, 0));
+						note_check[1][j].setForeground(new Color(0, 255, 0));
+					}
+				} else if (e.getStateChange() == ItemEvent.DESELECTED) {
+					if (e.getSource() == note_check[1][j]) {
+						note_check[1][j].setBackground(new Color(238, 238, 238));
+						note_check[1][j].setForeground(new Color(0, 0, 0));
+					}
+				}
+			}
+
+			for (int j = 0; j < 20; j++) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					if (e.getSource() == note_check[2][j]) {
+						note_check[2][j].setBackground(new Color(255, 255, 0));
+						note_check[2][j].setForeground(new Color(100, 0, 255));
+					}
+				} else if (e.getStateChange() == ItemEvent.DESELECTED) {
+					if (e.getSource() == note_check[2][j]) {
+						note_check[2][j].setBackground(new Color(238, 238, 238));
+						note_check[2][j].setForeground(new Color(0, 0, 0));
+					}
+				}
+			}
+
+			for (int j = 0; j < 20; j++) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					if (e.getSource() == note_check[3][j]) {
+						note_check[3][j].setBackground(new Color(0, 255, 0));
+						note_check[3][j].setForeground(new Color(255, 0, 0));
+					}
+				} else if (e.getStateChange() == ItemEvent.DESELECTED) {
+					if (e.getSource() == note_check[3][j]) {
+						note_check[3][j].setBackground(new Color(238, 238, 238));
+						note_check[3][j].setForeground(new Color(0, 0, 0));
+					}
+				}
+			}
+
+			for (int j = 0; j < 20; j++) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					if (e.getSource() == note_check[4][j]) {
+						note_check[4][j].setBackground(new Color(0, 100, 255));
+						note_check[4][j].setForeground(new Color(255, 100, 0));
+					}
+				} else if (e.getStateChange() == ItemEvent.DESELECTED) {
+					if (e.getSource() == note_check[4][j]) {
+						note_check[4][j].setBackground(new Color(238, 238, 238));
+						note_check[4][j].setForeground(new Color(0, 0, 0));
+					}
+				}
+			}
+
+			for (int j = 0; j < 20; j++) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					if (e.getSource() == note_check[5][j]) {
+						note_check[5][j].setBackground(new Color(0, 0, 255));
+						note_check[5][j].setForeground(new Color(255, 50, 0));
+					}
+				} else if (e.getStateChange() == ItemEvent.DESELECTED) {
+					if (e.getSource() == note_check[5][j]) {
+						note_check[5][j].setBackground(new Color(238, 238, 238));
+						note_check[5][j].setForeground(new Color(0, 0, 0));
+					}
+				}
+			}
+
+			for (int j = 0; j < 20; j++) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					if (e.getSource() == note_check[6][j]) {
+						note_check[6][j].setBackground(new Color(100, 0, 255));
+						note_check[6][j].setForeground(new Color(255, 255, 0));
+					}
+				} else if (e.getStateChange() == ItemEvent.DESELECTED) {
+					if (e.getSource() == note_check[6][j]) {
+						note_check[6][j].setBackground(new Color(238, 238, 238));
+						note_check[6][j].setForeground(new Color(0, 0, 0));
+					}
+				}
+			}
+
+		}
+	}
+
+	class Control_ButtonEvent implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			Sound sound = new Sound();
+			String action = e.getActionCommand();
+			
+			int count = 0;
+			
+			try {
+				if (action == "PLAY") {
+					for (int i = 0; i < 7; i++) {
+						for (int j = 0; j < 20; j++) {
+							if(note_check[i][j].isSelected() == true) {
+								
+							}
+						}
+					}
+					for(int i = 0; i < 20; i++) {
+//						note_check[count][i]
+						count++;
+						Thread.sleep(1000);
+					}
+					
+				} else if (action == "STOP") {
+
+				} else if (action == "PAUSE") {
+
+				}
+
+			} catch (Exception ae) {
+				JOptionPane.showMessageDialog(null, "실행중 오류 발생");
+			}
+		}
 
 	}
 }
@@ -178,7 +437,5 @@ public class PianoMaInGUI {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Piano p = new Piano();
-
 	}
-
 }
